9b7a18cc1ff35e47c4fb1a563948382d5ed6b0d5
apple <omxite@gmail.com>
2023-01-30 11:53:42 +0500

    New Commit

--- src/Api/Axios.js
+++ src/Api/Axios.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
     // baseURL: 'http://127.0.0.1:8000/api/'
-    baseURL: 'https://dwf.walnuthash.com/api/'
+    // baseURL: 'https://dwf.walnuthash.com/api/'
 });
